02d9cf84503696491de507585e0e0c55ec07f8f7
Daniel Hernandez <danielandres65@gmail.com>
2024-07-09 13:10:12 -0500

    Logos added + CSS changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 <div>
-    <h1>Brebe.</h1>
+    {{-- <h1>Brebe.</h1> --}}
+    <img src="{{ asset('img/logo-1.png') }}" width="300" class="text-center" alt="">
     <div id="button-container" class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
         <a href="{{ route('user.register') }}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
             Regístrate
